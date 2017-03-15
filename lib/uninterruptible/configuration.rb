0e3b9eb4da1dc73ec77a7a45cdd35a7043b864a3
Dan Wentworth <dan@atechmedia.com>
2017-03-15 11:46:19 +0000

    fall back to PID_FILE env variable

--- lib/uninterruptible/configuration.rb
+++ lib/uninterruptible/configuration.rb
@@ -5,27 +5,28 @@ module Uninterruptible
   class Configuration
     attr_writer :bind_port, :bind_address, :pidfile_path, :start_command
 
-    # Available TCP Port for the server to bind to (required).
+    # Available TCP Port for the server to bind to (required). Falls back to environment variable PORT if set.
     def bind_port
       @bind_port || ENV["PORT"] || raise ConfigurationError, "You must configure a bind_port"
     end
 
-    # Address to bind the server to (defaults to +::+)
+    # Address to bind the server to (defaults to +::+). Falls back to environment variable BIND_ADDRESS if set.
     def bind_address
       @bind_address || ENV["BIND_ADDRESS"] || "::"
     end
 
-    # Command that should be used to reexecute the server. Note that +bundle exec+ will be automatically added.
+    # Location to write the pid of the current server to. If blank pidfile will not be written. Falls back to
+    # environment variable PID_FILE if set.
+    def pidfile_path
+      @pidfile_path || ENV["PID_FILE"]
+    end
+
+    # Command that should be used to reexecute the server (required). Note: +bundle exec+ will be automatically added.
     #
     # @example
     #   rake app:run_server
     def start_command
       @start_command || raise ConfigurationError, "You must configure a start_command"
-    end
-
-    # Location to write the pid of the current server to. If blank pidfile will not be written.
-    def pidfile_path
-      @pidfile_path
     end
   end
 end
---
+++ spec/configuration_spec.rb
@@ -0,0 +1,5 @@
+include 'spec_helper'
+
+RSpec.describe Uninterruptible::Configuration do
+
+end
